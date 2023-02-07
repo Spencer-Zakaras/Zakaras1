7bf0c43fdb206f74cbc84eb5ea5f30a766ec4f5c
Spencer-Zakaras <122484249+Spencer-Zakaras@users.noreply.github.com>
2023-02-07 14:58:26 -0600

    Add files via upload
    This is the UPDATED version of this assignment with all documentation requirements.

--- Zakaras1/Program.cs
+++ Zakaras1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Spencer Zakaras\nCSC 346\nAssignment 1\nDue: 2-8-23");
+﻿/********************************************************************
+*** NAME : Spencer Zakaras
+*** CLASS : CSc 346 S01
+*** ASSIGNMENT : 1
+*** DUE DATE : 2/8/23
+*** INSTRUCTOR : GAMRADT 
+*********************************************************************
+*** DESCRIPTION : This program prints out my name, class, assignment, and due date
+********************************************************************/
+
+Console.WriteLine("Spencer Zakaras\nCSC 346\nAssignment 1\nDue: 2-8-23"); //print to console command with name, class, assignment, & due date
